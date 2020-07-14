206d3091355c659ea0a69d38570bd22df2719349
kunwu3 <krisvictor3@gmail.com>
2020-07-14 15:54:19 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -e
 # based on https://gist.github.com/corny/7a07f5ac901844bd20c9
 
-echo -e "400\n4\n10\n" | /root/CloudflareScanner
+echo -e "400\n4\n10\n" | /opt/update/CloudflareScanner
 
-cat result.csv | tail -n +2 | sort -t ',' -k 6 -g | tail -n 1 | awk -F, '{ curl -fsS "http://ipv4.dynv6.com/api/update?hostname=maopan.dns.army&token=5w7QNyKnnyyD_4fQ43YcpaghmzKctm&ipv4=$1" }'
+ip=`cat result.csv | tail -n +2 | sort -t ',' -k 6 -g | tail -n 1 | awk -F, '{ print $1 }'`
+
+curl -fsS "http://ipv4.dynv6.com/api/update?hostname=maopan.dns.army&token=5w7QNyKnnyyD_4fQ43YcpaghmzKctm&ipv4=$ip"
